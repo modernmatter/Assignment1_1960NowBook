d37f23a6b20faa154e48539ae781c333b5a122f2
modernmatter <aj@modernmatter.com>
2017-10-04 18:57:39 -0400

    Initial commit - add project files

---
+++ js/main.js
@@ -0,0 +1,53 @@
+// Set up the nav scrolling effect
+// $("nav").onePageNav();
+
+var menu = $("nav ul");
+
+// Toggle the menu when clicked
+$("#menu-icon").click(function() {
+	menu.slideToggle();
+});
+
+// Fix bug where menu disappears if you open and close it on a small screen
+// When I resize the window
+$(window).resize(function() {
+	var width = $(window).width();
+
+	// if window width is > 768 and the menu is hidden
+	if(width > 768 && menu.is(":hidden")) {
+		menu.removeAttr("style");
+	}
+
+});
+
+// Add parallax (stellar.js)
+$.stellar();
+
+// Add wow.js animations
+new WOW().init();
+
+// <script type="text/javascript">
+	$(window).ready(function() {
+		$('#magazine').turn({
+							display: 'double',
+							acceleration: true,
+							gradients: !$.isTouch,
+							elevation:50,
+							when: {
+								turned: function(e, page) {
+									/*console.log('Current view: ', $(this).turn('view'));*/
+								}
+							}
+						});
+	});
+	
+	
+	$(window).bind('keydown', function(e){
+		
+		if (e.keyCode==37)
+			$('#magazine').turn('previous');
+		else if (e.keyCode==39)
+			$('#magazine').turn('next');
+			
+	});
+// </script>
